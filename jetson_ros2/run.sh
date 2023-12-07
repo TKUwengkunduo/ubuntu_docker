4c1a3e6ed093fad4f4a32c55833ab1a5c6f2e860
TKUwengkunduo <wengkunduo@gmail.com>
2023-12-07 11:53:46 +0800

    jetson usb

--- jetson_ros2/run.sh
+++ jetson_ros2/run.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
+################################# PARAMETERS #################################
+# Get script path
 file_dir="$(dirname $0)"
 
 # Get WorkSpace parameters
 workspace=$(readlink -f "${0}")
 workspace=${workspace%_ws*}
-workspace_name=${workspace##*/}_ros2_cuda
+workspace_name=${workspace##*/}
 workspace_path=${workspace}"_ws"
 
 # Docker image and container name
@@ -16,17 +18,64 @@ container_name=${workspace_name}
 SCRIPT_DIR="$(dirname "$(readlink -f "$0")")"
 CONFIG_PATH="$SCRIPT_DIR/config/shell/terminator/config"
 
-xhost +
+# Start sharing xhost
+# You can also comment it, if you report an error
+# xhost +local:root
 
+################################# FUNCTIONS #################################
+################################
+# Get the GraphicsCard label
+# Arguments:
+# - $1 (Boolean) - Debug mode
+# Returns:
+# --gpus all is NVIDIA GraphicsCard
+# "" is not supported
+################################
+function get_graph_card() {
+    graphics_card=$(lspci | grep VGA)
 
-docker run --runtime nvidia \
-    -it --rm --network=host \
-    -e DISPLAY=$DISPLAY \
-    -e QT_X11_NO_MITSHM=1 \
-    -v /tmp/.X11-unix/:/tmp/.X11-unix \
-    -e XAUTHORITY=${HOME}/.Xauthority \
-    -v ${HOME}/.Xauthority:/home/${user}/.Xauthority \
-    -v /run/user/$(id -u)/:/run/user/$(id -u) \
-    -v ./config/shell/terminator/config:/home/${USER}/.config/terminator/config \
-    -v ${workspace_path}:${HOME}/work \
-    --name $container_name $(id -un)/${image_name}
+    if ${1}; then
+        echo $graphics_card
+    fi
+
+    graphics_card=${graphics_card##*:}
+    graphics_card=${graphics_card#* }
+    graphics_card=${graphics_card%% *}
+    graphics_card=$(echo $graphics_card | tr [a-z] [A-Z])
+
+    if [ $graphics_card == "NVIDIA" ]; then
+        echo "--gpus all"
+    else
+        echo ""
+    fi
+}
+
+################################
+# run docker container
+# Arguments:
+# - $1 (String) - GPU paramters
+################################
+docker_run() {
+    docker run --rm \
+        --net=host \
+        --ipc=host \
+        $1 \
+        --runtime nvidia \
+        --privileged \
+        -v /tmp/.X11-unix:/tmp/.X11-unix:rw \
+        -v ${HOME}/.Xauthority:/home/${user}/.Xauthority \
+        -v ${workspace_path}:${HOME}/work \
+        -v /dev:/dev \
+        -v ./config/shell/terminator/config:/home/${USER}/.config/terminator/config \
+        -v /run/user/$(id -u)/:/run/user/$(id -u) \
+        -e DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/$(id -u)/bus \
+        -e XAUTHORITY=${HOME}/.Xauthority \
+        -e DISPLAY=$DISPLAY \
+        -e QT_X11_NO_MITSHM=1 \
+        -it --name $container_name $(id -un)/${image_name}
+}
+
+#################################### MAIN ####################################
+gpu_flag="$(get_graph_card false)"
+docker_run "${gpu_flag}"
+
